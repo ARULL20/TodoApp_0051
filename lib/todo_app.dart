ef7af01e21eef53716a98490003f1e3360111c82
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-03-20 10:25:46 +0700

    membuat TodoAppState

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -1,1 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_picker/bottom_picker.dart';
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
